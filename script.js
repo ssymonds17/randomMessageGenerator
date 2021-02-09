54b0ec1f15b7cd4d299d2c23632c6b60267f006f
Samuel Symonds <samuelsymonds2017@gmail.com>
2021-02-09 15:08:58 +0000

    Create example output and initialise necessary arrays to store data

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log('first line');
+
+const firstNameArray = [];
+const secondNameArray = [];
+const locationArray = [];
+const heightArray = [];
+const positionArray = [];
+const teamArray = [];
+
+// const exampleSentence = `${fullName} was born in ${location} in ${year}. He was ${height} and was a ${position}. He played primarily for the ${team}, won ${titleNumber} championships.`
